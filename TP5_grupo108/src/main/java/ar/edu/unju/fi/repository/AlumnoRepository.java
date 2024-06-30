3ba4315455c2906e15b7219686db60d36332a81c
Naim Apaza <naimlautaroapaza@gmail.com>
2024-06-30 20:47:33 -0300

    Subida de repository de alumno, carrera, docente y materia

---
+++ TP5_grupo108/src/main/java/ar/edu/unju/fi/repository/AlumnoRepository.java
@@ -0,0 +1,9 @@
+package ar.edu.unju.fi.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import ar.edu.unju.fi.model.Alumno;
+
+public interface AlumnoRepository extends JpaRepository<Alumno, Long> {
+
+}
